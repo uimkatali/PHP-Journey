bd44999cbfa1fea5cc3de3cd8af095d9ac005fb5
uimkatali <b8bf6b5@gmail.com>
2025-04-05 17:19:15 +0300

    expressions in php; what is an expression, with examples, check code;

---
+++ typecasting/expressions.php
@@ -0,0 +1,22 @@
+<?php
+
+echo '<br/>';
+echo 'EXPRESSIONS';
+echo '<br/>';
+
+$x = 5; #this is an expression
+
+$y = $x; #this is also an expression
+#an expression is anything with an assignment operator because it evaluates something with a value
+
+echo '<br/>';
+
+#comparing one variable to another
+
+$z = $x === $y; #thi returns a value
+$g = sum($x, $y); #this is an expression, it returns a value
+
+echo '<br/>';
+
+
+echo '<br/>';
