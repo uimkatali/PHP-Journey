072c6c5947ff66ed917632fdd659b99340964292
uimkatali <b8bf6b5@gmail.com>
2025-04-21 19:50:31 +0300

    some exercises for the logic

--- FormWithInput/form.php
+++ FormWithInput/form.php
@@ -11,8 +11,8 @@
     </h1>
     <br/>
     <form action="form.php" method="get">
-        <label for="name">Enter your name: </label>
-        <input type="text" name="name" id="name" required >
+        <label for="sth">Enter your name: </label>
+        <input type="text" name="sth" id="sth" required >
         <br/>
         <input type="submit" value="Submit" >
     </form>
@@ -31,10 +31,93 @@
         <input type="submit" value="Check" >
     </form>
 
+    <h1/>
+
+    <div>
+        <?php
+        $i = 0;
+        while($i <= 10){
+            echo $i++;
+        
+        }
+        ?>
+    </div>
+    <h1/>
+
+    <div>
+    <?php 
+    $x = 0;
+        do{
+            echo $x++;
+        }while($x <= 12);
+    ?>
+    </div>
+
+    <br/>
+
+    <div>
+        <?php for($z=0; print $z, $z<=$x; $z++){
+            echo $z;
+        }
+        ?>
+    </div>
+
+    <div>
+        <?php
+            //foreach is used for arrays or objects
+            $userInfo = ['Catalin', 'CS', 'not a specialist in PHP yet'];
+            foreach($userInfo as $key => $item){
+                echo $item . "<br/>";
+            }
+        ?>
+    </div>
+
+    <br/>
+    <div>
+        <?php 
+            $user = [
+                'name' => "Catalin",
+                'username' => "catalinm",
+                'skills' => ['PHP', 'Laravel', 'React', 'JavaScript', 'C#'],
+            ];
+
+            foreach($user as $key => $item){
+                if(is_array($item)){
+                    foreach($item as $skill){
+                        echo $skill . '-';
+                    }
+                }else{
+                    echo $item;
+                }
+                echo '<br/>';
+            }
+
+        ?>
+    </div>
+<br/>
+    <form method="post">
+        <div style="padding:13px;">
+            <label for="switchCase">Choose a status</label>
+            <br/>
+            <select style="padding:13px;" name="statuses" id="switchCase" >
+                <Option value="Default" >--Select a status--</Option>
+                <Option value="Paid" >Paid</Option>
+                <Option value="Declined" >Declined</Option>
+                <Option value="Pending" >Pending</Option>
+            </select>
+            <input type="submit" value="Select" >
+        </div>
+    </form>
+    
+
     <?php 
         if($_SERVER["REQUEST_METHOD"] == "GET"){
-            $name = htmlspecialchars($_GET["name"]);
-            echo "<h3> Hello, " . $name . " !<h3/>";
+            if(isset($_GET["sth"]) && !is_null($_GET["sth"])){
+                $name = htmlspecialchars($_GET["sth"]);
+                echo "<h3> Hello, " . $name . " !<h3/>";
+            }else{
+                return null;
+            }
         }
         if($_SERVER["REQUEST_METHOD"] == "POST"){
 
@@ -64,10 +147,56 @@
                 echo "Sum of $firstNumber and $secondNumber equals: " . $sum;
 
             }
-            
 
+            if(isset($_POST["statuses"]) && $_POST["statuses"] != "Default" && !is_null($_POST["statuses"])){
+                $significantValues = $_POST["statuses"];
+                switch($significantValues){
+                    case 'Paid':
+                        echo "Paid";
+                        break;
+                    case 'Declined':
+                        echo "Declined";
+                        break;
+                    case 'Pending':
+                        echo "Pending";
+                        break;
+                    default:
+                        echo "Unknown Payment Status";
+                }
+            }else{
+                $defaultValue = $_POST["statuses"];
+                echo "You should select a significant value, you can't go further with " . $defaultValue;
+            }
+            echo '<br/>';
 
+            #php 8 switch case alternative - match
+            if(isset($_POST["statuses"]) && $_POST["statuses"] != "Default" && !is_null($_POST["statuses"])){
+                $significantValues = $_POST["statuses"];
 
+                $display = match($significantValues){
+                    'Paid' => 'Paid',
+                    'Declined'=>'Declined',
+                    'Pending' => 'Pending',
+                    'Default' => 'Default matching case',
+                };
+                switch($significantValues){
+                    case 'Paid':
+                        echo "Paid";
+                        break;
+                    case 'Declined':
+                        echo "Declined";
+                        break;
+                    case 'Pending':
+                        echo "Pending";
+                        break;
+                    default:
+                        echo "Unknown Payment Status";
+                }
+            }else{
+                $defaultValue = $_POST["statuses"];
+                echo "You should select a significant value, you can't go further with " . $defaultValue;
+            }
+    
         }
     ?>
 
